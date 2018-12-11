d1a07399bbf84b5bb3be0c141eaf7156e542602e
DwightCarterJr <dwightc@thewaveint.com>
2018-12-11 14:29:01 -0500

    change id to token

--- src/_helpers/auth-header.js
+++ src/_helpers/auth-header.js
@@ -1,8 +1,8 @@
 export function authHeader() {
     // return authorization header with jwt token
     let user = JSON.parse(localStorage.getItem('user'));
-    if (user && user.id) {
-        return { 'Authorization': 'Bearer ' + user.id };
+    if (user && user.token) {
+        return { 'Authorization': 'Bearer ' + user.token };
     } else {
         return {};
     }
