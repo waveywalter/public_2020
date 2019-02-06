4fce301f220e26934d42be4f2e318a86f88bd34e
DwightCarterJr <carter91jr@gmail.com>
2019-02-06 16:11:54 -0500

    changes to styling and button nothing serious

--- src/_pages/TinymceFormbuilder.vue
+++ src/_pages/TinymceFormbuilder.vue
@@ -1,51 +1,61 @@
 <template>
-<div>
-<topheader></topheader>
-<sidebar></sidebar>
- <div id="page-wrapper">
+  <div>
+    <topheader></topheader>
+    <sidebar></sidebar>
+    <div id="page-wrapper">
       <div class="container-fluid">
-        <div class="clearfix">
-          <div class="container white-box">
-            <h1>Form Title: <input type="text" name="FirstName" value=""></h1>
-    <tinymce></tinymce>
-    <input type="submit" value="Submit">
-
+        <div data-v-7ad1cecc class="col-lg">
+          <div data-v-7ad1cecc class="row bg-title">
+            <h4 data-v-7ad1cecc class="page-title">Form Builder</h4>
+          </div>
+        </div>
+        <div class="row">
+          <div class="container white-box col-sm-12">
+            <h1>
+              Form Title:
+              <input type="text" name value>
+            </h1>
+            <tinymce></tinymce>
+            <div class="col-lg-2 col-sm-4 col-xs-12">
+              <input
+                class="btn btn-success  upload-botton"
+                type="submit"
+                value="Submit"
+              >
+            </div>
           </div>
         </div>
       </div>
     </div>
-    </div>
+  </div>
 </template>
 
 
 <script>
-
-import tinymce from '../components/layoutComponents/tinymce'
-import topheader from '../components/layoutComponents/TopHeader'
-import sidebar from '../components/layoutComponents/SideBar'
-import formBuilder from '../formBuilder/formBuilder'
+import tinymce from "../components/layoutComponents/tinymce";
+import topheader from "../components/layoutComponents/TopHeader";
+import sidebar from "../components/layoutComponents/SideBar";
+import formBuilder from "../formBuilder/formBuilder";
 export default {
-    name:"tinymceformbulder",
-        data(){
-            return {
-
-            }
-        },
-        components:{
-            tinymce,
-            topheader,
-            sidebar,
-            formBuilder
-        }
-}
+  name: "tinymceformbulder",
+  data() {
+    return {};
+  },
+  components: {
+    tinymce,
+    topheader,
+    sidebar,
+    formBuilder
+  }
+};
 </script>
 
 <style scoped>
-#user_image_uploader_button{
-  display:none;
+#user_image_uploader_button {
+  display: none;
 }
-.upload-botton{
-    display: inline-table;
-    cursor: pointer;
+.upload-botton {
+  display: inline-table;
+  cursor: pointer;
 }
 </style>
