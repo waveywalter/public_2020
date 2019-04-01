4b267e96362172a5594820a102c8a45b348baa49
DwightCarterJr <carter91jr@gmail.com>
2019-04-01 08:41:08 -0400

    css changes to form display

--- src/_pages/affiliatespage.vue
+++ src/_pages/affiliatespage.vue
@@ -12,7 +12,7 @@
 import affiliateApplication from '../components/affiliateComponents/affiliateApplication'
 
     export default {
-        name: "ownerHome",
+        name: "affiliatespage",
         components: {
             affiliateApplication
         }
